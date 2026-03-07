fb4239e9d8b574bcb783706f94337f2175f944a2
Ved Patel <veds28112004@gmail.com>
2026-03-07 18:44:35 -0500

    feat: update layout and components for header, add search bar and user authentication

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,15 @@
+import MaxLogo from "./MaxLogo";
+import SearchBar from "./SearchBar";
+import UserAuth from "./UserAuth";
+
 export default function Header() {
-    return (
-        <header className="bg-[#000000] flex items-center ">
-            <div><h1 >Max</h1></div>
-            <div>sdvsd</div>
-            <div>sdvsd</div>
-        </header>
-    )
+  return (
+    <header className="bg-[#000000] grid grid-cols-3 items-center">
+      <MaxLogo />
+
+      <SearchBar />
+
+      <UserAuth />
+    </header>
+  );
 }
